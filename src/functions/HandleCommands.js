23f31a9aa272acda33f9fa268bc6a94c7108fb8f
KJ Richmond <kilgamesh@outlook.com>
2023-12-20 21:43:45 -0500

    chore: Improved comments + cmd handler improvements

--- src/functions/HandleCommands.js
+++ src/functions/HandleCommands.js
@@ -1,44 +1,57 @@
 const { REST, Routes } = require('discord.js');
 const fs = require('fs');
 
-const guildId = "your guild id";
-const clientId = "your client id"
+const guildId = ""; // Your guild id
+const clientId = ""; // Your bot's client id
 
 module.exports = (client) => {
-	client.handleCommands = async (commandFolders, path) => {
+// Function to handle command arrays
+	client.handleCommandsArray = async (commandFolders, path) => {
 		client.commandArray = [];
-		for(folder of commandFolders) {
+		// Loop through all the folders in the commands folder
+		// Loop through each command folder
+		for (commandFolder of commandFolders) {
 			const commandFiles = fs.readdirSync(`${path}/${folder}`).filter(file => file.endsWith('.js'));
-			for(file of commandFiles) {
+		// Process each command file
+			for (commandFile of commandFiles) {
 				const command = require(`../commands/${folder}/${file}`);
 				client.commands.set(command.data.name, command);
 				client.commandArray.push(command.data.toJSON());
 			}
 		}
+	};
+
 
 		const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
-
-		(async() => {
-			try {
-				console.log('Started refreshing (/) commands.');
-
+	// Event listener for client
+	(async() => {
+		// Attempt to register commands
+		try {
+			console.log('Started refreshing (/) commands.');
+			if (!clientId) {
+				console.error('No client id provided.');
+				return;
+			}
+		
+			// Remove all global commands
+			// await rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: [] });
+		
+			// Remove all guild commands
+			// await rest.put(Routes.applicationCommands(clientId), { body: [] });
+			
+			if (guildId) {
 				await rest.put(
-					// Clears global commands
-					// Routes.applicationCommands(clientId), { body: []},
-					
-					// Adds commands to global scope (all servers)
-					// Routes.applicationCommands(clientId), { body: client.commandArray},
-					
-					// Adds commands to guild scope (only your server)
-					Routes.applicationGuildCommands(clientId, guildId), {
-						body: client.commandArray
-					},
+					Routes.applicationGuildCommands(clientId, guildId), { body: client.commandArray }
 				);
+			} else {
+				await rest.put(
+						Routes.applicationCommands(clientId), { body: client.commandArray }
+					);
+				}
 
 				console.log('Successfully reloaded (/) commands.');
-			} catch (error) {
-				console.error(error);
-			}
-		})();
-	}
+		} catch (error) {
+			console.error(error);
+		}
+	})();
 }
